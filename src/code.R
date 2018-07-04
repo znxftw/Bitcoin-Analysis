9b9c93be4abae02931991b7ea29fd3ce8eb17c24
Vishnu Bhagyanath <vishnu2101@gmail.com>
2018-07-04 09:05:17 +0530

    Added RF and XGBoost

--- src/code.R
+++ src/code.R
@@ -6,6 +6,8 @@
 # install.packages('lubridate')
 # install.packages('xgboost')
 # install.packages('anytime')
+# install.packages('caret')
+#
 
 # Importing the libraries
 suppressMessages(library(randomForest))
@@ -14,6 +16,7 @@ suppressMessages(library(scales))
 suppressMessages(library(lubridate))
 suppressMessages(library(xgboost))
 suppressMessages(library(anytime))
+suppressMessages(library(caret))
 
 # Reading the file and splitting the dataset
 dataset = read.csv('BTC_DATA.csv')
@@ -49,15 +52,16 @@ rf_regressor = randomForest(x = training_set[2:32],
 rf_pred = predict(rf_regressor, test_set[2:32])
 
 ggplot() +
-  geom_line(aes(x = test_set$Date, y = test_set$MKPRU),
-            colour = 'red',
-            show.legend = TRUE) +
-  geom_line(aes(x = test_set$Date, y = rf_pred),
-            colour = 'green',
-            show.legend = TRUE) +
-  ggtitle('Random Forest Regression') +
+  geom_line(aes(x = test_set$Date, 
+                y = test_set$MKPRU,
+                colour = "red")) +
+  geom_line(aes(x = test_set$Date, 
+                y = rf_pred,
+                colour = 'green')) +
+  ggtitle('Random Forest Regression (500 trees)') +
   xlab('Date') +
-  ylab('Price ($)')
+  ylab('Price ($)')+
+  scale_color_discrete(name = "Legend", labels = c("Random Forest Prediction", " True Value"))
 
 # XGBoost
 xg_regressor = xgboost(data = as.matrix(training_set[2:32]), 
@@ -68,12 +72,41 @@ xg_pred = predict(xg_regressor,
                   newdata = as.matrix(test_set[2:32]))
 
 ggplot() +
-  geom_line(aes(x = test_set$Date, y = test_set$MKPRU),
-                colour = 'red',
-                show.legend = TRUE) +
-  geom_line(aes(x = test_set$Date, y = xg_pred),
-                colour = 'green',
-                show.legend = TRUE) +
-  ggtitle('XGBoost') +
+  geom_line(aes(x = test_set$Date, 
+                y = test_set$MKPRU,
+                colour = "red")) +
+  geom_line(aes(x = test_set$Date, 
+                y = xg_pred,
+                colour = 'green')) +
+  ggtitle('XGBoost (300 rounds)') +
   xlab('Date') +
-  ylab('Price ($)')
+  ylab('Price ($)')+
+  scale_color_discrete(name = "Legend", labels = c("XGBoost", " True Value"))
+
+# Calculating errors
+
+RMSE = function(m, o){
+  sqrt(mean((m - o)^2))
+}
+MAE = function(m, o)
+{
+  mean(abs(m - o))
+}
+
+y_mean = mean(test_set$MKPRU)
+
+rf_rmse = RMSE(rf_pred,test_set$MKPRU)
+xg_rmse = RMSE(xg_pred,test_set$MKPRU)
+
+rf_mae = MAE(rf_pred,test_set$MKPRU)
+xg_mae = MAE(xg_pred,test_set$MKPRU)
+
+rf_rmse_p = rf_rmse / y_mean * 100
+xg_rmse_p = xg_rmse / y_mean * 100
+
+rf_mae_p = rf_mae / y_mean * 100
+xg_mae_p = xg_mae / y_mean * 100
+
+# Performance Tuning
+# rf_tuning = train(form = MKPRU ~ . - Date, data = training_set, method = 'rf')
+
